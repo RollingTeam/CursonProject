7a68bbb8aa6c5c35dbea3570c1a554d2408cc4cb
Florencia Pistan Esparza <flor.pistan.esparza@gmail.com>
2020-10-22 20:44:14 -0300

    actualizando repo remoto

--- js/Modulos/Clases/Categorias.js
+++ js/Modulos/Clases/Categorias.js
@@ -1,19 +1,19 @@
 class Categoria {
-    constructor (categoria, estado){
-        this.categoria = categoria;
+    constructor(nombre, estado) {
+        this.nombre = nombre;
         this.estado = estado;
     }
 }
 
 let categorias = [];
 
-let categoriaUno = new Categoria ("Programación", "Activa");
+let categoriaUno = new Categoria("Programación", "Activa");
 
-let categoriaDos = new Categoria ("Arte", "Activa");
+let categoriaDos = new Categoria("Arte", "Activa");
 
-let categoriaTres = new Categoria ("Idiomas", "Activa");
+let categoriaTres = new Categoria("Idiomas", "Activa");
 
-let categoriaCuatro = new Categoria ("Carpintería", "Inactiva");
+let categoriaCuatro = new Categoria("Carpintería", "Inactiva");
 
 categorias.push(categoriaUno, categoriaDos, categoriaTres, categoriaCuatro);
 
@@ -21,3 +21,5 @@ categorias.push(categoriaUno, categoriaDos, categoriaTres, categoriaCuatro);
 
 localStorage.clear();
 localStorage.setItem("categorias", JSON.stringify(categorias));
+
+//Cargar Categorías
