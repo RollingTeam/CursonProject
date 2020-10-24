c08c638db364756d8d491e76c0a2da6ad5b358c6
Belen Neme <belenneme1@gmail.com>
2020-10-24 16:34:21 -0300

    Modificando en Destcados Cursos

--- js/Modulos/Funciones/destacados.js
+++ js/Modulos/Funciones/destacados.js
@@ -1,6 +1,5 @@
 let destacados = JSON.parse(localStorage.getItem("cursos"));
 
-
 function agregarDestacados() {
     console.log("entramos a la funcion")
     let cards = document.getElementById("tarjetas");
@@ -8,19 +7,22 @@ function agregarDestacados() {
         console.log("entramos al for")
         while (i < 3) {
             console.log("entramos al while")
-            let cuerpoCard = `<div class="card">
+            let cuerpoCard = `<div class="col-md-4 mt-3"
+            <div class="card ">
             <img class="card-img-top img-fluid" src="https://picsum.photos/200/200/?blur"
                 alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">${destacados[i.nombreCurso]}</h5>
-                <span class="badge badge-pill badge-warning">${destacados[i.nivelCurso]}</span>
-                <p class="card-text p-1">${destacados[i.descripcionCurso]}</p>
-                <button type="button" class="btn btn-danger">Más Info</button>
+                <h5 class="card-title">${destacados[i].nombreCurso}</h5>
+                <span class="badge badge-pill badge-warning">${destacados[i].nivelCurso}</span>
+                <p class="card-text p-1">${destacados[i].descripcionCurso}</p>
+                <button onclick="mostrarModalCurso(${destacados[i].idCurso})" class="btn btn-primary-curso float-right" data-toggle="modal" data-target="#modalVerMas">
+                Ver más
+                </button>
+                </div>
             </div>
         </div>`;
             i += 1;
             cards.innerHTML += cuerpoCard;
-            cards += cuerpoCard;
         }
     }
 }
