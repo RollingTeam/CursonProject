ba9bb40dd0fbf5386dc50d7aa6872842cf216e04
Belen Neme <belenneme1@gmail.com>
2020-10-25 18:11:59 -0300

    Cambiando rutas

--- js/Modulos/Funciones/accesoAdmin.js
+++ js/Modulos/Funciones/accesoAdmin.js
@@ -7,12 +7,12 @@ validacionAdminPage();
 
 function validacionAdminPage(){
     let pagina = location.pathname;
-    if (pagina ==="/C:/Users/belen/OneDrive/Documentos/RollingCode/3I/CursonProject/adminHome.html"){
+    if (pagina ==="/CursonProject/adminHome.html"){
         if(userLogueados===""){
-            location = "/C:/Users/belen/OneDrive/Documentos/RollingCode/3I/CursonProject/index.html";
+            location = "/CursonProject/index.html";
         }else if (usuarioLogueado[0].userRole != "1") {
             console.log("No eres Administrador")
-            location = "/C:/Users/belen/OneDrive/Documentos/RollingCode/3I/CursonProject/index.html";
+            location = "/CursonProject/index.html";
         }
     }
 };
