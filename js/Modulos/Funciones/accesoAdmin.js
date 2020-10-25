3ceec620929f5e9a25a5a460d36a75a3fd64bd53
Florencia Pistan Esparza <flor.pistan.esparza@gmail.com>
2020-10-25 19:45:00 -0300

    validar acceso: funcionando

--- js/Modulos/Funciones/accesoAdmin.js
+++ js/Modulos/Funciones/accesoAdmin.js
@@ -7,12 +7,12 @@ validacionAdminPage();
 
 function validacionAdminPage(){
     let pagina = location.pathname;
-    if (pagina ==="/C:/Users/belen/OneDrive/Documentos/RollingCode/3I/CursonProject/adminHome.html"){
+    if (pagina ==="/C:/Users/Florencia/OneDrive/Documentos/Programaci%C3%B3n/Proyectos/CursOn/CursonProject/adminHome.html"){
         if(userLogueados===""){
-            location = "/C:/Users/belen/OneDrive/Documentos/RollingCode/3I/CursonProject/index.html";
+            location = "/C:/Users/Florencia/OneDrive/Documentos/Programaci%C3%B3n/Proyectos/CursOn/CursonProject/index.html";
         }else if (usuarioLogueado[0].userRole != "1") {
             console.log("No eres Administrador")
-            location = "/C:/Users/belen/OneDrive/Documentos/RollingCode/3I/CursonProject/index.html";
+            location = "/C:/Users/Florencia/OneDrive/Documentos/Programaci%C3%B3n/Proyectos/CursOn/CursonProject/index.html";
         }
     }
 };
