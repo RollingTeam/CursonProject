c792feb54265d7292dc1b2f4886713c30b87cdda
Florencia Pistan Esparza <flor.pistan.esparza@gmail.com>
2020-10-26 11:06:44 -0300

    probando ruta para validación en github pages

--- js/Modulos/Funciones/accesoAdmin.js
+++ js/Modulos/Funciones/accesoAdmin.js
@@ -7,12 +7,12 @@ validacionAdminPage();
 
 function validacionAdminPage(){
     let pagina = location.pathname;
-    if (pagina ==="/C:/Users/Florencia/OneDrive/Documentos/Programaci%C3%B3n/Proyectos/CursOn/CursonProject/adminHome.html"){
+    if (pagina ==="/CursonProject/adminHome.html"){
         if(userLogueados===""){
-            location = "/C:/Users/Florencia/OneDrive/Documentos/Programaci%C3%B3n/Proyectos/CursOn/CursonProject/index.html";
+            location = "/CursonProject/";
         }else if (usuarioLogueado[0].userRole != "1") {
             console.log("No eres Administrador")
-            location = "/C:/Users/Florencia/OneDrive/Documentos/Programaci%C3%B3n/Proyectos/CursOn/CursonProject/index.html";
+            location = "/CursonProject/";
         }
     }
 };
